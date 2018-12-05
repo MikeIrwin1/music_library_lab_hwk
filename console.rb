c49662075a7e99c0dd7240ec9b3c63a9efdc3874
Michael Irwin <mike.irwin1@gmail.com>
2018-12-05 15:32:34 +0000

    read all complete

--- console.rb
+++ console.rb
@@ -3,10 +3,15 @@ require_relative('models/artist')
 require_relative('models/album')
 
 artist1 = Artist.new({'name' => 'Nirvana'})
+artist2 = Artist.new({'name' => 'Van Halen'})
 artist1.save
+artist2.save
 
 album1 = Album.new({ 'title' => 'Nevermind', 'genre' => 'grunge'})
 album1.save
+
+artists = Artist.all()
+albums = Album.all()
 
 binding.pry
 nil
